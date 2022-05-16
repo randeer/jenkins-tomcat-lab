47e68deb65c20ec834c26092618dd1f48d507232
randeer <randeerlalanga92@gmail.com>
2022-05-16 11:49:09 +0530

    randeer

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -40,4 +40,10 @@ public class TestGreeter {
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
   }
+    @Test
+  public void greetShouldIncludeGreetingMessage() {
+    String someone = "Randeer Lalanga";
+
+    assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
+  }
 }
